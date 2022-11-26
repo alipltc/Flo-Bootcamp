da12a4595a76bfb393b0407787e2b96aabb0e209
Ali PALTACI <alipltc@hotmail.com>
2022-11-26 16:08:26 +0300

    Update class.php

--- Bootcamp_Ödev4/class.php
+++ Bootcamp_Ödev4/class.php
@@ -10,10 +10,10 @@ class TcKimlik {
         $dizim = array();
         $tek  = 0;
         $cift = 0;
-        $tümtoplam = 0;
+        $tumtoplam = 0;
         for ($i=0;$i<11;$i++) {
             $dizim[] += substr($tcno,$i,1);
-            $tümtoplam += substr($tcno,$i,1);
+            $tumtoplam += substr($tcno,$i,1);
             if ($i%2 == 0) {
                 $tek  += substr($tcno,$i,1);
             } else {
@@ -23,7 +23,7 @@ class TcKimlik {
         $tek  -= $dizim[10];
         $cift -= $dizim[9];
         $islem_bir = (($tek*7)- $cift)%10;//3. durum için
-        $islem_iki = ($tümtoplam - $dizim[10])%10; //4.durum için
+        $islem_iki = ($tumtoplam - $dizim[10])%10; //4.durum için
         if (($dizim[0] != 0) and ($islem_bir == $dizim[9]) and ($islem_iki == $dizim[10])) {
             return 1;
         } else {
